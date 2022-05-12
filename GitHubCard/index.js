7725c6eff6bfcb6262752884de095a45a28ac09f
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-05-12 00:24:24 -0400

    class lists added

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -45,9 +45,13 @@ const userBio = document.createElement('p');
 
 
 
+card.classList.add("card");
+cardImg.src = userObj.src;
+cardInfo.classList.add("card-info");
+name.classList.add("name");
+userName.classList.add("username");
+profileLink.href = userObj.href;
 }
-
-
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
